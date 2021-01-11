92502d0ce2d89ff56b050d5f1e17b6035858c5bd
Michael Yuen <iam@mikey.baby>
2021-01-10 21:06:02 -0500

    Consolidate styles

--- components/FlippingNumber.js
+++ components/FlippingNumber.js
@@ -16,98 +16,70 @@ const flip = keyframes`
 const FlippingNumberContainer = styled.div`
   position: relative;
 
-  .bottom {
-    background: rgb(52, 53, 81);
-    background: linear-gradient(
-        rgba(188, 167, 216, 0.1),
-        rgba(72, 66, 95, 0.1) 50%,
-        rgb(52, 53, 81)
-      ),
-      rgb(52, 53, 81);
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-    box-shadow: 0px 7px 2px rgb(26, 26, 37);
-  }
-
-  .top {
-    background: linear-gradient(to bottom, rgb(44, 43, 70), rgb(59, 60, 95));
-
+  div {
     display: flex;
     justify-content: center;
-    backface-visibility: hidden;
-    overflow: hidden;
-
-    position: absolute;
     left: 0;
-    top: 0;
-    height: 50%;
+    position: absolute;
     width: 100%;
 
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    box-shadow: inset 0 2px rgba(0, 0, 0, 0.9),
-      inset 0 3px 0 rgba(255, 255, 255, 0.4);
-  }
-
-  .bottom.current {
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    height: 50%;
-    overflow: hidden;
-    width: 100%;
-
-    border-top: 2px solid black;
-  }
-
-  .flipper {
-    position: absolute;
-    left: 0;
-    top: 0;
-    height: 50%;
-    width: 100%;
-
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    animation: ${({ animationDuration, shouldAnimate }) =>
-      shouldAnimate
-        ? css`
-            ${flip} ${animationDuration}ms ease-in forwards
-          `
-        : "none"};
-
-    transform-origin: bottom;
-    transform-style: preserve-3d;
-
-    .bottom,
-    .top {
-      display: flex;
-      justify-content: center;
-      backface-visibility: hidden;
+    &:not(.flipper) {
       overflow: hidden;
+    }
 
-      position: absolute;
-      left: 0;
+    &:not(.bottom.current) {
       top: 0;
-      height: 100%;
-      width: 100%;
     }
 
-    .bottom {
-      align-items: flex-end;
-      transform: rotateX(-0.5turn);
-    }
-
-    .top {
+    &.top {
+      backface-visibility: hidden;
       background: linear-gradient(to bottom, rgb(44, 43, 70), rgb(59, 60, 95));
-
       border-top-left-radius: 10px;
       border-top-right-radius: 10px;
-      box-shadow: inset 0 2px rgba(0, 0, 0, 0.9),
+      box-shadow: 0px -2px 5px rgb(26, 26, 37), inset 0 2px rgba(0, 0, 0, 0.9),
         inset 0 3px 0 rgba(255, 255, 255, 0.4);
+      height: 50%;
+    }
+
+    &.bottom {
+      align-items: flex-end;
+      background: linear-gradient(
+          rgba(188, 167, 216, 0.1),
+          rgba(72, 66, 95, 0.1) 50%,
+          rgb(52, 53, 81)
+        ),
+        rgb(52, 53, 81);
+      border-bottom-left-radius: 10px;
+      border-bottom-right-radius: 10px;
+      box-shadow: 0px 7px 2px rgb(26, 26, 37);
+
+      &.current {
+        height: 50%;
+        border-top: 2px solid black;
+        bottom: 0;
+      }
+    }
+
+    &.flipper {
+      animation: ${({ animationDuration, shouldAnimate }) =>
+        shouldAnimate
+          ? css`
+              ${flip} ${animationDuration}ms ease-in forwards
+            `
+          : "none"};
+      height: 50%;
+      transform-origin: bottom;
+      transform-style: preserve-3d;
+
+      .bottom,
+      .top {
+        backface-visibility: hidden;
+        height: 100%;
+      }
+
+      .bottom {
+        transform: rotateX(-0.5turn);
+      }
     }
   }
 `;
